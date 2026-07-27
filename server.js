0b66b1a9a7bf7fa4fd9ac9daf16cdf657e8d5120
Rapa2026 <levymelanie@hotmail.com>
2026-07-27 23:47:07 +0100

    Update server port and improve logging

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT ||10000;
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
@@ -46,4 +46,6 @@ res.status(500).json({ success: false, message: 'Email failed'});
 }
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => { 
+  console.log(`Server running on port ${PORT}`)
+})  
